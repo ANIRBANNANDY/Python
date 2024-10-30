eeed7126d6e5f51b43e8c294efb65d363b54e649
ANIRBANNANDY <newton.ani@gmail.com>
2024-10-30 16:21:13 +0530

    Update Flask.py

--- Flask.py
+++ Flask.py
@@ -1,30 +1,40 @@
 from flask import Flask, render_template, request, redirect, url_for
 import psutil
+import os
+from datetime import datetime
 
 app = Flask(__name__)
 
-# Route to display the process list
-@app.route('/')
-def list_processes():
-    processes = []
-    for proc in psutil.process_iter(['pid', 'name', 'username', 'cpu_percent', 'memory_percent']):
-        try:
-            proc_info = proc.info
-            processes.append(proc_info)
-        except (psutil.NoSuchProcess, psutil.AccessDenied):
-            continue  # Skip processes that are no longer available
-    return render_template('process_list.html', processes=processes)
+# List of processes to monitor
+TARGET_PROCESSES = ["perl.exe", "python.exe", "sas.exe"]
 
-# Route to kill a process by PID
-@app.route('/kill/<int:pid>', methods=['POST'])
+def get_process_details():
+    process_details = []
+    for proc in psutil.process_iter(attrs=['pid', 'name', 'username', 'exe', 'cmdline', 'create_time']):
+        if proc.info['name'] in TARGET_PROCESSES:
+            process_details.append({
+                'pid': proc.info['pid'],
+                'name': proc.info['name'],
+                'user': proc.info['username'],
+                'full_path': proc.info['exe'],
+                'cmdline': ' '.join(proc.info['cmdline']),
+                'start_time': datetime.fromtimestamp(proc.info['create_time']).strftime('%Y-%m-%d %H:%M:%S')
+            })
+    return process_details
+
+@app.route('/')
+def index():
+    processes = get_process_details()
+    return render_template('index.html', processes=processes)
+
+@app.route('/kill/<int:pid>')
 def kill_process(pid):
     try:
-        process = psutil.Process(pid)
-        process.terminate()  # Terminate the process
-        process.wait(timeout=3)  # Wait for the process to terminate
-    except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.TimeoutExpired):
-        pass  # Handle cases where the process cannot be terminated
-    return redirect(url_for('list_processes'))
+        p = psutil.Process(pid)
+        p.terminate()  # You can use p.kill() if you want to force kill
+        return redirect(url_for('index'))
+    except Exception as e:
+        return str(e)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
