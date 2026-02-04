3235fe9a80566af125a301cc67947b529e023654
ANIRBANNANDY <newton.ani@gmail.com>
2026-02-05 00:08:01 +0530

    Update Schedule.bat

--- Schedule.bat
+++ Schedule.bat
@@ -1,24 +1,27 @@
 @echo off
 setlocal
-:: Configuration: Set your project path and Python location
+:: Configuration
 set "PROJECT_DIR=C:\ProcessMonitor"
 set "PYTHON_EXE=python.exe"
+:: CHANGE THIS PER SERVER:
+set "MY_HOSTNAME=server1.xyz.com"
 
 cd /d "%PROJECT_DIR%"
 
-:: --- START AGENT ---
-tasklist /FI "WINDOWTITLE eq AgentProcess" | find /i "python.exe" >nul
+:: --- CHECK HUB (Only if this is Server 1) ---
+:: We look for a python process that has "hub.py" in the command line
+powershell -Command "Get-CimInstance Win32_Process -Filter \"Name = 'python.exe'\" | Where-Object {$_.CommandLine -like '*hub.py*'} | Exit"
 if %errorlevel% neq 0 (
-    echo Starting Agent.py...
-    start "AgentProcess" /min %PYTHON_EXE% agent.py
+    echo [$(get-date)] Starting Hub.py in background...
+    start /B "" %PYTHON_EXE% hub.py %MY_HOSTNAME%
 )
 
-:: --- START HUB (Only on Server 1) ---
-:: Remove these lines if you are deploying to Servers 2-6
-tasklist /FI "WINDOWTITLE eq HubProcess" | find /i "python.exe" >nul
+:: --- CHECK AGENT ---
+:: We look for a python process that has "agent.py" in the command line
+powershell -Command "Get-CimInstance Win32_Process -Filter \"Name = 'python.exe'\" | Where-Object {$_.CommandLine -like '*agent.py*'} | Exit"
 if %errorlevel% neq 0 (
-    echo Starting Hub.py...
-    start "HubProcess" /min %PYTHON_EXE% hub.py
+    echo [$(get-date)] Starting Agent.py in background...
+    start /B "" %PYTHON_EXE% agent.py
 )
 
 exit
